af49375649002e51e380dfe8ae86b56973d44303
Rajat Jain <rajjain8@Rajats-MacBook-Pro.local>
2022-08-28 02:48:18 +0530

    data server get and post

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -2,22 +2,39 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  fetch('http://localhost:8000/counter/1/')
+ .then(response => response.json())
+ .catch(error => console.error('Error:', error))
+ .then(response => console.log('Success:', JSON.stringify(response)));
+
+ function updateCounter() {
+  console.log('hellllooo');
+
+  let data = { 
+    "id": 10, 
+    "title": "json-server is not bad. It's great!", 
+    "author": "Dwight Shrute" 
+  }
+  
+   fetch('http://localhost:8000/counter/', {
+    method: "POST",
+    headers: {
+    "Content-Type": "application/json",
+   },
+    body: JSON.stringify(data),
+    })
+    .then(response => response.json())
+    .catch(error => console.error('Error:', error))
+    .then(response => console.log('Success:', JSON.stringify(response)));
+ }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+    <button onClick={updateCounter}>
+      First
+    </button>
     </div>
   );
 }
